7f0558f937e847d7ce80d2db1ebbf5420e10d300
Falt-b <goodoldflats@gmail.com>
2022-11-26 12:38:49 -0500

    change to work with new code

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pygame
 import iso_grid
+import input_handler
 from sys import exit
 
 WIDTH = 1280
 HEIGHT = 880
-FPS = 10
+FPS = 60
 BG_COLOR = (20, 20, 20)
 
 
@@ -16,8 +17,10 @@ def main():
 
     center = pygame.Vector2(WIDTH / 2, 0)
 
-    test_grid = iso_grid.Iso_Grid((64, 64), center)
-    iso_grid.generate_grid(20, 20, test_grid, "images/test_tile.png", 2)
+    test_grid = iso_grid.Iso_Grid(20, 20, 64, 64, center[0], 0, center[0], 0)
+    test_grid.generate_grid("images/test_tile.png", 2)
+
+    mouse = input_handler.Mouse_Handler()
 
     while True:
         clock.tick(FPS)
@@ -27,14 +30,21 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     return False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse.get_pressed()
+            if event.type == pygame.MOUSEBUTTONUP:
+                mouse.get_released()
 
         display.fill(BG_COLOR)
 
         cords = test_grid.screen_to_grid(pygame.Vector2(pygame.mouse.get_pos()))
-        tile_index = int(cords.x + cords.y * 20)
+        tile_index = int(cords[0] + cords[1] * 20)
         if tile_index > 399:
             tile_index = 399
-        test_grid.slect_tile(tile_index)
+
+        test_grid.offset += mouse.click_drag()
+
+        test_grid.sprites()[tile_index].offset.y = -10
 
         test_grid.draw_grid(display)
 
