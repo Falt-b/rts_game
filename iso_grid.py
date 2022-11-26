3262bccfa3564c1b2d30bce03022a649d772ab49
Falt-b <goodoldflats@gmail.com>
2022-11-26 12:36:49 -0500

    cleanup messy code

--- iso_grid.py
+++ iso_grid.py
@@ -8,54 +8,62 @@ class Tile(pygame.sprite.Sprite):
         super().__init__(*groups)
         self.image = image
         self.rect = self.image.get_rect(topleft=position)
-        self.offset = pygame.Vector2(0, 0)
+        self.offset = pygame.Vector2()
 
 
 class Iso_Grid(pygame.sprite.Group):
-    def __init__(self, tile_size: tuple, grid_offset: tuple, *sprites) -> None:
+    def __init__(
+        self,
+        grid_width: int,
+        grid_height: int,
+        tile_width: int,
+        tile_height: int,
+        x_offset: float,
+        y_offset: float,
+        center_x: float,
+        center_y: float,
+        *sprites
+    ) -> None:
         super().__init__(*sprites)
-        self.tile_size = tile_size
-        self.half_width = tile_size[0] / 2
-        self.quarter_height = tile_size[1] / 4
-        self.offset = pygame.Vector2(grid_offset)
+        self.grid_width = grid_width
+        self.grid_height = grid_height
+        self.hgrid_width = grid_width * 0.5
+        self.hgrid_height = grid_height * 0.5
+        self.tile_width = tile_width
+        self.tile_height = tile_height
+        self.htile_width = tile_width * 0.5
+        self.qtile_height = tile_height * 0.25
+        self.offset = pygame.Vector2((x_offset, y_offset))
+        self.center = (center_x, center_y)
+
+    def generate_grid(self, image: str, scale: int = 1, colorkey: tuple = (0, 0, 0)):
+        img = load_image(image, scale, colorkey)
+        for y in range(self.grid_height):
+            for x in range(self.grid_width):
+                Tile((x, y), img, self)
 
     def screen_to_grid(self, position: pygame.Vector2):
-        x = floor(
-            0.5 * (position.x / self.half_width + position.y / self.quarter_height)
+        set_position = (
+            position
+            + pygame.Vector2(0, self.qtile_height * 0.5)
+            + (pygame.Vector2(self.center) - self.offset)
         )
-        y = floor(
-            0.5 * (-position.x / self.half_width + position.y / self.quarter_height)
+        grid_x = set_position.x / self.htile_width
+        grid_y = set_position.y / self.qtile_height
+        return pygame.Vector2(
+            floor((grid_x + grid_y) * 0.5) - 10,
+            floor((-grid_x + grid_y) * 0.5) + 10,
         )
-        return pygame.Vector2(x - 10, y + 10)
 
     def grid_to_screen(self, tile: Tile):
         return pygame.Vector2(
-            (tile.rect.topleft[0] - tile.rect.topleft[1]) * self.half_width
-            - self.half_width,
-            (tile.rect.topleft[0] + tile.rect.topleft[1]) * self.quarter_height,
+            ((tile.rect.topleft[0] - tile.rect.topleft[1]) * self.htile_width)
+            - self.htile_width,
+            (tile.rect.topleft[0] + tile.rect.topleft[1]) * self.qtile_height,
         )
-
-    def slect_tile(self, index: int):
-        for sprite in self.sprites():
-            sprite.offset.y = 0
-        self.sprites()[index].offset.y = -10
 
     def draw_grid(self, surface: pygame.Surface):
         for tile in self.sprites():
             surface.blit(
                 tile.image, self.grid_to_screen(tile) + self.offset + tile.offset
             )
-
-
-def generate_grid(
-    width: int,
-    height: int,
-    group: Iso_Grid,
-    image: str,
-    scale: int = 1,
-    colorkey: tuple = (0, 0, 0),
-):
-    img = load_image(image, scale, colorkey)
-    for y in range(height):
-        for x in range(width):
-            Tile((x, y), img, group)
